3bd1fe2d79237a135ca32a7f6cfa9fcb7b8d2aeb
Per <defayetteperry@gmail.com>
2020-02-27 18:35:59 -0500

    Cleaned up code. TODO: Check text input for invalid chars.

--- input.js
+++ input.js
@@ -1,15 +1,10 @@
 window.onload = () => {
-  console.log("onload...");
   const buttonID = document.getElementById("btn");
   const rangeInput = document.getElementById("speedSlider");
   const speedDisplay = document.getElementById("speedDisplay");
   const input = document.getElementById("inputbox");
-  rangeInput.value = 1;
-  speedDisplay.innerHTML = "<br>Speed: Fast!";
-  btn.addEventListener("click", btnToggle);
-  rangeInput.addEventListener('input', updateSpeed);
-  sessionStorage.clear();
-  input.value = "";
+  
+  initialize();
 
   var worker;
   let useWorker = false;
@@ -25,34 +20,23 @@ window.onload = () => {
     if(useWorker == false){
       useWorker = true;
       initWorker();
-      console.log("worker started...");
     }
     else{
      console.log("worker already running..."); 
     }
-    /*
-    if (typeof(worker) == "undefined") {
-      useWorker = true;
-      initWorker();
-      console.log("worker started...");
-    } else {
-      useWorker = false;
-      worker.postMessage({ cmd: "stop" });
-      //worker.terminate(); //removed since worker terminates itself using self.close()
-      worker = undefined; //reset worker
-    }
-    */
   }
   
   function initWorker() {
     var input = inputbox.value;
     
+    //if go button is pressed when text field is blank...
     if(input == "")
       input = "scramble / de-scramble";
     
     console.log(input);
     
     //todo: check for char legitimacy
+    
     //if good, store
     if(typeof(Storage) !== "undefined"){
       console.log('Web storage available.');
@@ -67,31 +51,18 @@ window.onload = () => {
         worker = new Worker(window.URL.createObjectURL(workerData));
       }
       
+      //on every postmessage from worker:
       worker.onmessage = function(event) {
         document.getElementById("title").innerHTML = event.data;
+        console.log(event.data);
         
-        if (useWorker == true) {          
-          console.log(event.data);
-          if(event.data == input){
-            console.log("Input found. Stopping worker.");
-            clear();
-            worker.postMessage({cmd: "stop"});
-            worker = undefined;
-            useWorker = false;
-          }
-          
-          //document.getElementById("title2").innerHTML = event.data;
-        } else if (useWorker == false) {
-          //document.getElementById("title2").innerHTML = event.data;
-          console.log("last received num from worker: " + event.data);
-          
-          //if good, store
-          if (typeof Storage !== "undefined") {
-            console.log("Web storage available.");
-            sessionStorage.setItem("lastNum", event.data);
-          } else {
-            console.log("Web storage unavailable.");
-          }
+        //if posted word from worker = stored input, stop the worker and reset
+        if(event.data == input){
+          console.log("Input found. Stopping worker.");
+          worker.postMessage({cmd: "stop"});
+          worker = undefined;
+          useWorker = false;
+          clear();
         }
       };
     } else {
@@ -104,8 +75,8 @@ window.onload = () => {
       speed: rangeInput.value
     });
   }
-
-  function updateSpeed() {
+  
+  rangeInput.oninput = function(){
     switch(rangeInput.value){
       case '1':{
         speedDisplay.innerHTML = "<br>Speed: Fast!";
@@ -119,11 +90,7 @@ window.onload = () => {
         speedDisplay.innerHTML = "<br>Speed: " + rangeInput.value;
         break;
       }
-    }    
-  }
-  
-  rangeInput.oninput = function(){
-    speedDisplay.innerHTML = "<br>Speed: " + rangeInput.value;
+    }   
     console.log("rangeUpdated");
     
     if(useWorker != false){
@@ -131,94 +98,17 @@ window.onload = () => {
       console.log("Slider = " + rangeInput.value);
     }
   }
-};
-
-function initialize(){
-	var w;
-	initWebWorker();
-	initSlider();
-	clear();
-}
-
-function submitInput(){
-	var input = document.getElementById("inputbox").value;
-	
-	if(input == "")
-		input = "scramble / de-scramble";
-	
   
-	console.log(input);
-
-	//todo: check for char legitimacy
-
-	//if good, store
-	if(typeof(Storage) !== "undefined"){
-		console.log('Web storage available.');
-		sessionStorage.setItem("in", input);
-  }else{
-    console.log('Web storage unavailable.');
+  function clear(){
+    sessionStorage.clear();
+    input.value = "";
   }
 
-  //initWebWorker();
-	if(typeof(w) == "undefined"){
-		w = new Worker("scramble.js");
-	}
-
-	var scramble = [];
-	for(var i = 0; i < input.length; i++){
-		scramble.push("x");
-	}
-
-	w.postMessage({'cmd': 'initInput', 'msg': scramble.join(''), 'in': input});
-}
-
-function initWebWorker(){
-
-	//check for worker support, start new if available
-	if(typeof(Worker) !== "undefined"){
-		console.log("Web worker support.");
-		
-		if(typeof(w) == "undefined"){
-			w = new Worker("scramble.js");
-		}
-		else{
-	  	console.log('No web worker support.');
-		}
-	}	
-
-	//event listener for web worker
-	w.addEventListener('message', function(e){
-		//output
-		document.getElementById('title').innerHTML = e.data;
-
-		//check for correctness, if matched stop worker and clear data
-		if(e.data == sessionStorage.getItem('in')){
-			console.log("Input found. Stopping worker.");
-			clear();
-			w.terminate();
-			w = undefined;
-			initWebWorker();
-		}
-	}, false);
-}
-
-function initSlider(){
-	//slider bar
-	var slider = document.getElementById("speedSlider");
-	var output = document.getElementById("speedDisplay");
-	output.innerHTML = "<br>" + "Speed: " + slider.value;
-	//w.postMessage({'cmd': 'sliderInput', 'msg': this.value});
-
-
-	slider.oninput = function(){
-		output.innerHTML = "<br>" + "Speed: " + this.value;
-		if(w !== "undefined"){
-			w.postMessage({'cmd': 'sliderInput', 'msg': this.value});
-		}
-	}
-}
-
-function clear(){
-	document.getElementById("inputbox").value = "";
-	sessionStorage.clear();
-}
+  function initialize(){
+    rangeInput.value = 1;
+    speedDisplay.innerHTML = "<br>Speed: Fast!";
+    btn.addEventListener("click", btnToggle);
+    sessionStorage.clear();
+    input.value = "";
+  }
+};
